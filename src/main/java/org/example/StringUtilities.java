c3fb7777f8ec2c58fc9d97cbff58f0c86a605f36
pattersonj5081 <150544502+pattersonj5081@users.noreply.github.com>
2025-06-10 22:13:04 -0500

    finished Classes and objects in Java: Practice 1.

--- src/main/java/org/example/StringUtilities.java
+++ src/main/java/org/example/StringUtilities.java
@@ -5,12 +5,37 @@ public class StringUtilities {
 
     // Add a static function called shortString that takes a String parameter and returns true if the string has fewer than 5
     // characters
+    public static boolean shortString(String input){
+        if (input.length() < 5){
+            return true;
+        }
+        return false;
+    }
 
     // Add a static function called firstLetter that takes a String parameter and returns the first character of the string
+    public static char firstLetter(String input){
+       return input.charAt(0);
+    }
 
     // Add a static function called censorAsparagus that takes a String parameter and returns the string with all instances of
     // "asparagus" (lowercase only) replaced with 4 stars: "****"
-
+    public static String censorAsparagus(String input){
+        String fourStar= input;
+        if (input.contains("asparagus")){
+            fourStar = input.replaceAll("asparagus", "****");
+        }
+        return fourStar;
+    }
     // Add a static function called bigger that takes two String parameters and returns the longer of the two strings. If the
     // strings are the same length, return the first string.
+    public static String bigger (String a, String b){
+        if (a.length() > b.length()){
+            return a;
+        } else if (b.length() > a.length()) {
+            return b;
+        }else{
+            return a;
+        }
+    }
+
 }
